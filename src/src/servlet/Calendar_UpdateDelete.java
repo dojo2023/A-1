439de8b96c6d8785216deea49fbb86e7038bf229
dojo2023-mahmudfariha <MAHMUDFARIHAplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-12 15:37:49 +0900

    calendar

---
+++ src/src/servlet/Calendar_UpdateDelete.java
@@ -0,0 +1,48 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Calendar_UpdateDelete
+ */
+@WebServlet("/Calendar_UpdateDelete")
+public class Calendar_UpdateDelete extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Calendar_UpdateDelete() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		/*if (session.getAttribute("id") == null) {
+			response.sendRedirect("/jiro_power/LoginServlet");
+			return;
+		}*/
+		// リクエストパラメータを取得する
+				request.setCharacterEncoding("UTF-8");
+				
+		// ページにフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/training_record.jsp");
+				dispatcher.forward(request, response);
+    }
+}
+
