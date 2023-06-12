93970076d11ae1545c0205d501d66de3bc10168d
dojo2023-mahmudfariha <MAHMUDFARIHAplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-12 14:39:30 +0900

    Update MypageServlet.java

--- src/src/servlet/MypageServlet.java
+++ src/src/servlet/MypageServlet.java
@@ -1,19 +1,22 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class MypageServlet
  */
-@WebServlet("/MypageServlet")
+@WebServlet("/jiro_power/src/servlet/MypageServlet")
 public class MypageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -25,17 +28,33 @@ public class MypageServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		/*if (session.getAttribute("id") == null) {
+			response.sendRedirect("/jiro_power/LoginServlet");
+			return;
+		}*/
+
+		// マイページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/jiro_power/mypage.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		/*if (session.getAttribute("id") == null) {
+			response.sendRedirect("/jiro_power/LoginServlet");
+			return;
+		}*/
+		// リクエストパラメータを取得する
+				request.setCharacterEncoding("UTF-8");
+    }
+}
+
 
 }
