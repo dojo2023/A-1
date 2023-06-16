33ff2d3a4fde8a2d4bee7e1a7e1e669e0737b5ea
dojo2023-hasegawaranma <130331165+dojo2023-hasegawaranma@users.noreply.github.com>
2023-06-16 15:32:27 +0900

    +ボタン再誕の儀

--- src/WebContent/js/training_record.js
+++ src/WebContent/js/training_record.js
@@ -1,3 +1,13 @@
-/**
- * 
- */
+const form = document.getElementById("form");
+const button = document.getElementById("button");
+
+form.addEventListener("input", update);
+form.addEventListener("change", update);
+function update() {
+  const isRequired = form.checkValidity();
+
+  if (isRequired) {
+    button.disabled = false;
+    return;
+  }
+}
